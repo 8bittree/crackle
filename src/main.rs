c5d831876a9540352e2d2979826df5b6cdb99d51
Jon Olin <github.jao@jon.tomolin.net>
2020-05-14 23:37:27 -0400

    add v0 (linear) and v1 (sinusoidal)

--- src/main.rs
+++ src/main.rs
@@ -16,3 +16,13 @@ fn main() {
 
     imgbuf.save("image.png").unwrap();
 }
+
+fn v0(x: u32, y: u32) -> (u32, u32) {
+    (x, y)
+}
+
+fn v1(x: u32, y: u32) -> (u32, u32) {
+    let xf = f64::from(x);
+    let yf = f64::from(y);
+    (xf.sin() as u32, yf.sin() as u32)
+}
